0ee120a28e87e388f2dfa295cfd27a0512dd8021
Matheus Cesconetto <mat.cesco@gmail.com>
2024-07-04 20:47:47 -0300

    grafico

--- venv/src/app.py
+++ venv/src/app.py
@@ -4,10 +4,10 @@ import re
 from dateutil import parser
 import matplotlib.pyplot as plt
 import numpy as np
-
+import smtplib
 
 # Carregar a planilha Excel
-tabela = pd.read_excel("Relacao_Produtos_e_Clientes_2024.xlsx")
+tabela = pd.read_excel("venv/src/Relacao_Produtos_e_Clientes_2024.xlsx")
 
 # Selecionar a coluna desejada
 coluna_produtos = 'Produto'  
@@ -76,13 +76,15 @@ for index, row in tabela.iterrows():
 for desconto in tabela[coluna_desconto]:
     if desconto >= 0:
         array_desconto.append(desconto)
+    else:
+        array_desconto.append(0)
 
 
 
 # Imprimir os primeiros 10 elementos de cada array para verificar
 print("Array de Produtos:", array_produto)
 # print("Array de Pagamentos:", array_pagamento)
-# print("Array de Regiões:", array_regiao)
+print("Array de Regiões:", array_regiao)
 print("Array de Valores de Venda:", array_valor)
 # print("Array de Equipes de Vendas:", array_eqvenda)
 # print("Array de Desconto:", array_desconto)
@@ -102,28 +104,47 @@ print("Array de Valores de Venda:", array_valor)
 #         print(f"Erro ao padronizar data: {e}")
 #         return None
 
-# # Aplica a padronização na coluna de datas
-# tabela['Data Padronizada'] = tabela[coluna_datas].apply(padronizar_data)
 
-# print(tabela[[coluna_datas, 'Data Padronizada']])
-# Agrupar valores por região
-plt.figure(figsize=(10, 6))
+df_produtos = pd.DataFrame({
+    coluna_produtos: array_produto,
+})
+df_valor = pd.DataFrame({
+    coluna_valor: array_valor
+    })
+df_regiao = pd.DataFrame({
+    coluna_regiao: array_regiao
+})
 
-# Ajuste de posição para barras laterais
-posicoes = np.arange(len(array_produto))
+df = pd.DataFrame({
+    coluna_produtos: array_produto,
+    coluna_pagamento: array_pagamento,
+    coluna_regiao: array_regiao,
+    coluna_valor: array_valor,
+    coluna_eqvenda: array_eqvenda,
+    coluna_cliente: array_cliente,
+    coluna_desconto: array_desconto
+})
 
-# Plotagem das barras por região
-for regiao in set(array_regiao):
-    indices = [i for i, value in enumerate(array_regiao) if value == regiao]
-    produtos_regiao = [array_produto[i] for i in indices]
-    valores_regiao = [array_valor[i] for i in indices]
-    plt.barh(produtos_regiao, valores_regiao, height=0.2, label=f'Vendas - {regiao}')
+df2 = pd.DataFrame({
+    coluna_produtos: array_produto,
+    coluna_regiao: array_regiao,
+    coluna_valor: array_valor,
+})
 
-# Ajustes de legenda e labels
-plt.xlabel('Valores de Venda ($)')
-plt.ylabel('Produtos')
-plt.title('Valores de Venda por Produto e Região')
-plt.legend()  # Adiciona a legenda com base nos rótulos
+df2_filtrado = df2[df2[coluna_valor] != 0]
 
-plt.tight_layout() 
+# Calculando a média dos valores de venda por produto e região
+pivot_df = df2_filtrado.pivot_table(index=coluna_produtos, columns='Região', values=coluna_valor, aggfunc=lambda x: x.mean() if (x != 0).any() else None)
+
+# Plotando o gráfico de barras agrupadas
+pivot_df.plot(kind='bar', figsize=(12, 6))
+plt.xlabel(coluna_produtos)
+plt.ylabel(coluna_valor)
+plt.title(f'Gráfico de Barras Agrupadas (Média dos Valores de {coluna_valor})')
+plt.legend(title='Região')
+
+# Exibindo o gráfico
+plt.tight_layout()
 plt.show()
+
+email_sender = 'mat.cesco@gmail.com'
